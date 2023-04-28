194ac32ace6ecef52bedbf22b0d5d0a1f8f63ac4
kajaveaniruddha <aakajave@gmail.com>
2023-04-28 16:46:19 +0530

    removing node

--- space_time_signal.js
+++ space_time_signal.js
@@ -11,14 +11,18 @@ function calc() {
   const ctx = document.getElementById("myChart");
   let cosdata = [];
   let ydata = [];
-  for (var i = 0; i <= 360; i++) {
-    cosdata.push(amplitude * Math.cos(i));
-    // ydata.push(i);
+//   for (var i = 0; i <= 360; i++) {
+//     cosdata.push(amplitude * Math.cos(i));
+//     // ydata.push(i);
+//   }
+  
+  for (var i = 0; i <= 4*Math.PI; i++) {
+    ydata.push();
   }
   new Chart(ctx, {
     type: "line",
     data: {
-      labels: [0,90,180,240,300,360],
+      labels: [0,1,2,3,4,5,6],
       datasets: [
         {
           label: "Acos(wt-kx+phi)",
