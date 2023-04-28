903a9ffd7ce8f7eb97ae1ae6228052a0fca31428
kajaveaniruddha <aakajave@gmail.com>
2023-04-29 02:06:18 +0530

    friis and space time signal

--- friis_transmission.js
+++ friis_transmission.js
@@ -1,33 +1,120 @@
-// const selectElement = document.getElementById("selector");
-// console.log(selectElement.value);
-// selectElement.addEventListener("change", function () {
-//   if (selectElement.value == "freq") {
-//     console.log(selectElement.value);
-//     document.querySelector("#lambda").value = 0;
-//     document.getElementById("lambda").setAttribute("disabled", "true");
-//     document.getElementById("frequency").removeAttribute("disabled");
-//   } else if (selectElement.value == "lamb") {
-//     console.log(selectElement.value);
-//     document.querySelector("#frequency").value = 0;
-//     document.getElementById("frequency").setAttribute("disabled", "true");
-//     document.getElementById("lambda").removeAttribute("disabled");
-//   }
-// });
+const selectElement = document.getElementById("selector");
+console.log(selectElement.value);
+selectElement.addEventListener("change", function () {
+  if (selectElement.value == "none") {
+    document.querySelector("#lambda").value = "";
+    document.querySelector("#frequency").value = "";
+    document.getElementById("lambda").setAttribute("disabled", "true");
+    document.getElementById("frequency").setAttribute("disabled", "true");
+    document.querySelector("#answer").value = "";
+  } else if (selectElement.value == "freq") {
+    document.querySelector("#lambda").value = "";
+    document.querySelector("#frequency").value = "";
+    document.getElementById("lambda").setAttribute("disabled", "true");
+    document.getElementById("frequency").removeAttribute("disabled");
+    document.querySelector("#answer").value = "";
+  } else if (selectElement.value == "lamb") {
+    console.log(selectElement.value);
+    document.querySelector("#frequency").value = "";
+    document.querySelector("#lambda").value = "";
+    document.querySelector("#answer").value = "";
+    document.getElementById("frequency").setAttribute("disabled", "true");
+    document.getElementById("lambda").removeAttribute("disabled");
+  }
+});
 
-// function calc() {
-//   var Pt = parseFloat(document.getElementById("Pt").value);
-//   var distance = parseFloat(document.getElementById("distance").value);
-//   var Gt = parseFloat(document.getElementById("Gt").value);
-//   var Gr = parseFloat(document.getElementById("Gr").value);
-  
-//   var frequency = parseFloat(document.getElementById("frequency").value);
-//   document.getElementById("lambda").value = 300000000 / frequency;
-//   var lambda = parseFloat(document.getElementById("lambda").value);
-//   document.getElementById("frequency").value = 300000000 / lambda;
-  
-//   var output =
-//     (Pt * Gt * Gr * (lambda * lambda) * 0.00633257397) / (distance * distance);
-//   document.getElementById("answer").value = output;
-//   console.log(output);
-// }
-//testing
+function calc() {
+  var frequency, lambda;
+  var Pt = parseFloat(document.getElementById("Pt").value);
+  var distance = parseFloat(document.getElementById("distance").value);
+  var Gt = parseFloat(document.getElementById("Gt").value);
+  var Gr = parseFloat(document.getElementById("Gr").value);
+  if (selectElement.value == "freq") {
+    frequency = parseFloat(document.getElementById("frequency").value);
+    document.getElementById("lambda").value = 300000000 / frequency;
+    var output =
+      (Pt * Gt * Gr * Math.pow(3 * Math.pow(10, 8), 2) * 0.00633257397) /
+      (frequency * frequency * distance * distance);
+    document.getElementById("answer").value = output;
+  } else if (selectElement.value == "lamb") {
+    lambda = parseFloat(document.getElementById("lambda").value);
+    document.getElementById("frequency").value = 300000000 / lambda;
+    var output =
+      (Pt * Gt * Gr * (lambda * lambda) * 0.00633257397) /
+      (distance * distance);
+    document.getElementById("answer").value = output;
+  }
+
+  console.log(output);
+
+  // graph 1
+  const ctx = document.getElementById("myChart");
+  let cosdata = [];
+  let ydata = [];
+  var amplitude = Math.sqrt(Pt);
+  for (var i = 0; i <= 4 * Math.PI; i += Math.PI / 4) {
+    cosdata.push(amplitude * Math.cos(i));
+    var temp = i;
+    ydata.push(temp.toFixed(2));
+  }
+
+  const labels = ydata;
+  const data = {
+    labels,
+    datasets: [
+      {
+        data: cosdata,
+        label: "Pt v/s time",
+        // fill:true,
+        tension: 0.35,
+      },
+    ],
+  };
+
+  const config = {
+    type: "line",
+    data: data,
+    options: {
+      radius: 0,
+      responsive: true,
+    },
+  };
+
+  const myChart = new Chart(ctx, config);
+
+  // graph 2
+    let cosdata2 = [];
+    let ydata2 = [];
+    var amplitude=Math.sqrt(output);
+    console.log(amplitude);
+    for (var i = 0; i <= 4 * Math.PI; i += Math.PI / 4) {
+        cosdata2.push(amplitude * Math.cos(i));
+        var temp = i;
+        ydata2.push(temp.toFixed(2));
+        
+    }
+    
+    const labels2 = ydata2;
+    const data2 = {
+        labels2,
+        datasets: [
+            {     
+                data: cosdata2,
+                label: "Pr v/s time",
+                // fill:true,
+                tension: 0.35,
+            },
+        ],
+    };
+    
+    const config2 = {
+        type: "line",
+        data: data2,
+        options: {
+            radius: 0,
+            responsive: true,
+        },
+    };
+
+    const myChart2 = new Chart(document.querySelector("#myChart2"),config2);
+}
--- space_time_signal.js
+++ space_time_signal.js
@@ -2,40 +2,45 @@ function calc() {
   var amplitude = document.querySelector(".amplitude").value;
   var phase = document.querySelector(".phase").value;
   var lambda = document.querySelector(".lambda").value;
-  var omegatemp = (2 * 3.14) / lambda;
+  var time = document.querySelector(".time").value;
+  var ktemp = (2 * 3.14) / lambda;
+  let k = ktemp.toFixed(3);
+  var omegatemp = (2 * 3.14) / time;
   let omega = omegatemp.toFixed(3);
-  var result = `${amplitude}cos(kx-${omega}t+${phase})`;
+  var result = `${amplitude}cos(${k}x-${omega}t+${phase})`;
   document.querySelector(".answer").value = result;
 
   // graph
   const ctx = document.getElementById("myChart");
   let cosdata = [];
   let ydata = [];
-//   for (var i = 0; i <= 360; i++) {
-//     cosdata.push(amplitude * Math.cos(i));
-//     // ydata.push(i);
-//   }
-  
-  for (var i = 0; i <= 4*Math.PI; i++) {
-    ydata.push();
+  for (var i = 0; i <= 4 * Math.PI; i += Math.PI / 4) {
+    cosdata.push(amplitude * Math.cos(i));
+    var temp = i;
+    ydata.push(temp.toFixed(2));
   }
-  new Chart(ctx, {
-    type: "line",
-    data: {
-      labels: [0,1,2,3,4,5,6],
-      datasets: [
-        {
-          label: "Acos(wt-kx+phi)",
-          data: cosdata,
-        },
-      ],
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
+
+  const labels = ydata;
+  const data = {
+    labels,
+    datasets: [
+      {
+        data: cosdata,
+        labels: result,
+        // fill:true,
+        tension: 0.35,
       },
+    ],
+  };
+
+  const config = {
+    type: "line",
+    data: data,
+    options: {
+      radius: 0,
+      responsive: true,
     },
-  });
+  };
+
+  const myChart = new Chart(ctx, config);
 }
